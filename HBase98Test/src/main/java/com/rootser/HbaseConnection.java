98062acce045c804166619f1760c5fe1bed9a37f
john <jhancock1975@gmail.com>
2014-04-13 14:46:24 -0400

    test

---
+++ HBase98Test/src/main/java/com/rootser/HbaseConnection.java
@@ -0,0 +1,37 @@
+package com.rootser;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+
+import org.apache.hadoop.hbase.HColumnDescriptor;
+
+import org.apache.hadoop.hbase.HTableDescriptor;
+
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
+public class HbaseConnection {
+	public static void main(String[] args) throws IOException {
+		Configuration conf = new Configuration();
+		
+		HBaseConfiguration hc = new HBaseConfiguration(conf);
+
+		HTableDescriptor ht = new HTableDescriptor("User");
+
+		ht.addFamily(new HColumnDescriptor("Id"));
+
+		ht.addFamily(new HColumnDescriptor("Name"));
+
+		System.out.println("connecting");
+
+		HBaseAdmin hba = new HBaseAdmin(hc);
+
+		System.out.println("Creating Table");
+
+		hba.createTable(ht);
+
+		System.out.println("Done......");
+	}
+}
